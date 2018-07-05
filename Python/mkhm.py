4707616a17091d18871b1ed557fedc69bffc8d90
ktszk <stm435435@gmail.com>
2018-07-05 12:46:23 +0900

    fix mkhm.py

--- Python/mkhm.py
+++ Python/mkhm.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-from __future__ import print_function, division
-import numpy as np
 def mkhm(hop,rvec,klist,nlist):
+    import numpy as np
     """
     nr is number of hopping matrix
     no is number of orbitals
@@ -11,7 +10,7 @@ def mkhm(hop,rvec,klist,nlist):
     klist: list of k-point coordinate as [kx,ky,kz]
     nlist: list of number of k-mesh for each axis as [nqx,nqy,nqz]
     """
-    pi2=2*np.pi
+    pi2=2j*np.pi
     phase=[pi2*sum(r*k/float(n) for r,k,n in zip(rr,klist,nlist)) for rr in rvec] 
     en=np.array([[sum(hp*np.exp(ph)) for hp,ph in zip(hopp,phase)] for hopp in hop])
     return en
